4471ab037f2277d31818596c260392b9c3c6228d
Justin Oakley <jmoakle2@uncg.edu>
2019-02-25 22:57:43 -0500

    Updated CSC-526 Assignment 2

--- CSC-526_Assignment2/SemanticSimilarity.py
+++ CSC-526_Assignment2/SemanticSimilarity.py
@@ -1,4 +1,7 @@
-import csv,re
+import csv,re,math
+
+def sim_j(gene1, gene2):
+    return [len(set(x + y))/len(x + y) for x in gene1 for y in gene2]
 
 with open('ontology_ids_superclasses.tsv') as superclasses, open('semantic_similarity_ids.txt', 'r') as ids:
     supr_clses_reader = [(i[0], i[1].split(',')) for i in csv.reader(superclasses, delimiter='\t')]
@@ -6,13 +9,11 @@ with open('ontology_ids_superclasses.tsv') as superclasses, open('semantic_simil
     supr_clses = [row[1] for row in supr_clses_reader]
     ss_ids = []
     gene_names = []
-    for id in [re.split(" |,", row.rstrip("\n")) for row in ids]:
-        gene_names.append(id.pop(0))
-        for item in id:
-            if item in subclass:
-                item_index = subclass.index(item)
-                for sc in supr_clses[item_index]:
-                    id.append(sc)
-        ss_ids.append(id)
+    for ids in [re.split(" |,", row.rstrip("\n")) for row in ids]:
+        gene_names.append(ids.pop(0))
+        ss_ids.append([[x] for x in ids])
 
-    
+    ss_ids = [[x + supr_clses[subclass.index(x[0])] for x in gene] for gene in ss_ids]
+
+    print("Jaccard All Pairs similarity between GeneA and GeneC:", sim_j(ss_ids[0], ss_ids[2]))
+    print("Jaccard Best Pairs similarity between GeneA and GeneC:", sim_j(ss_ids[0], ss_ids[2]))
