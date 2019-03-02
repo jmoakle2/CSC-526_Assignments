c6bfc294f714a762d17b20ec1210547c8aafe293
Justin Oakley <jmoakle2@uncg.edu>
2019-03-01 22:08:23 -0500

    Updated CSC-526 Assignment 2

--- CSC-526_Assignment2/SemanticSimilarity.py
+++ CSC-526_Assignment2/SemanticSimilarity.py
@@ -16,20 +16,26 @@ def best_pairs_sim_j(all_pairs, original):
     return all_pairs
 
 # This method calculates the best pairs between the given genes using Resnik similarity.
-def best_pairs_sim_resnik(gene1, gene2):
-    lcs = []
+def best_pairs_sim_resnik(gene1, gene2, all_genes):
+    resnik_values = []
+    for identifier_list in [gene for gene in all_genes]:
+        for identifier in identifier_list:
+            num = 0
+            for id in [gene for gene in all_genes]:
+                if identifier in id:
+                    num += 1
+            resnik_values.append((identifier, 0 - math.log10(num / len(all_genes))))
+    resnik_values = set(resnik_values)
 
+    mica = []
     for x in gene1:
         for y in gene2:
             for x_item in x:
                 if x_item in y:
-                    lcs.append(x_item)
+                    mica.append(x_item)
                     break
 
-    values = [0 - math.log10((len([item for x in gene1 if item in x])
-                              + len([item for y in gene2 if item in y])) / 2) for item in lcs]
-
-    return list(zip(lcs, values))
+    return [value for value in resnik_values for x in mica if x in value[0]]
 
 # The following code reads the "ontology_ids_superclasses.tsv" file, which contains all the superclasses that were
 # retrieved from the Java program file, and the "semantic_similarity_ids.txt" file, which contains all the GO
@@ -62,7 +68,7 @@ with open('ontology_ids_superclasses.tsv') as superclasses, open('semantic_simil
     print("Best Pairs Average of GeneA and GeneC:", sum([x[1] for x in num_2]) / len(num_2))
 
 # 3. Best Pairs using Resnik Similarity
-    num_3 = best_pairs_sim_resnik(inferred[0], inferred[1])
+    num_3 = best_pairs_sim_resnik(inferred[0], inferred[1], [set([j for i in k for j in i]) for k in inferred])
 
     print("Best Pairs (Using Resnik Similarity) between GeneA and GeneB:", num_3)
     print("Best Pairs Average of GeneB and GeneC:", sum([x[1] for x in num_3]) / len(num_3))
