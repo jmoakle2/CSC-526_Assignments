d0b58c7e6922e35f98a0adc8fd6344d16b406b08
Justin Oakley <jmoakle2@uncg.edu>
2019-04-07 20:25:23 -0400

    Update CSC 526 Assignment 5

--- CSC-526_Assignment5/HypothesisTesting.py
+++ CSC-526_Assignment5/HypothesisTesting.py
@@ -5,12 +5,19 @@
 import pandas as pd, numpy as np
 from scipy import stats
 
+# Import data to a Pandas dataframe.
 S = pd.read_csv("SemanticSimilarityScores.tsv", sep='\t')
+
+# Take a sample size of 50 with replacement.
 sample = S.sample(n=50, replace=True)
+
+# Create a,b for each type of similarity using the Semantic Similarity Scores' columns.
 simj_a = sample['SimJ Score MACHINE']
 simj_b = sample['SimJ Score HUMAN']
 nic_a = sample['NIC Score MACHINE']
 nic_b = sample['NIC Score HUMAN']
+
+# Run statistic tests.
 t_simj, p_simj = stats.ttest_ind(simj_a, simj_b)
 t_nic, p_nic = stats.ttest_ind(simj_a, simj_b)
 
