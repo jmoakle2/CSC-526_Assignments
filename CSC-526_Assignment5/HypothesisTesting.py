9ab74c80a31866c1058947b1a2eee64b0acf49c6
Justin Oakley <jmoakle2@uncg.edu>
2019-04-08 13:53:02 -0400

    Updated CSC 526 Assignment 5

--- CSC-526_Assignment5/HypothesisTesting.py
+++ CSC-526_Assignment5/HypothesisTesting.py
@@ -18,8 +18,8 @@ nic_a = sample['NIC Score MACHINE']
 nic_b = sample['NIC Score HUMAN']
 
 # Run statistic tests.
-t_simj, p_simj = stats.ttest_ind(simj_a, simj_b)
-t_nic, p_nic = stats.ttest_ind(simj_a, simj_b)
+t_simj, p_simj = stats.ttest_rel(simj_a, simj_b)
+t_nic, p_nic = stats.ttest_rel(simj_a, simj_b)
 
 print("t-Score of Jaccard Similarity Scores: %s\np-Value of Jaccard Similiarity Scores %s" % (t_simj, p_simj))
 print("t-Score of Resnik Similarity Scores: %s\np-Value of Resnik Similiarity Scores %s" % (t_nic, p_nic))
